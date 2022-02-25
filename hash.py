40b8c976cdba59dba1aacf9e92e6f9a5a604b5aa
OddMood <oddmood.git@gmail.com>
2022-02-25 16:43:46 +0300

    Add Function 2

--- hash.py
+++ hash.py
@@ -9,14 +9,15 @@ def GetImageHash(image):
     hash = [1 if i >= treshold else 0 for i in arr.flatten()]
     return hash
 
-def main():
-    image = Image.open('test images\\akudama drive.png')
-    image1 = Image.open('test images\\akudama drive wo logo.png')
-    hash = GetImageHash(image)
+def GetDifferenceCoef(image1, image2):
     hash1 = GetImageHash(image1)
-    sim = np.count_nonzero(hash!=hash1)/len(hash)
-    print(hash)
-    print(hash1)
+    hash2 = GetImageHash(image2)
+    return np.count_nonzero(hash1!=hash2)/len(hash1)
+
+def main():
+    image1 = Image.open('test images\\akudama drive.png')
+    image2 = Image.open('test images\\akudama drive wo logo.png')
+    sim = GetDifferenceCoef(image1,image2)
     print(sim)
 
 if __name__ == "__main__":
