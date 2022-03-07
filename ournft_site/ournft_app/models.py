188e9bd8665c64e6edafa95606e120efb147d0c7
Maryam Khamzaeva <mrmkhmz@gmail.com>
2022-03-07 12:30:57 +0300

    Add accounts

--- ournft_site/ournft_app/models.py
+++ ournft_site/ournft_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    datetime = models.DateTimeField(verbose_name=u"Date", auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=u"Author", related_name="posts")
+    text = models.CharField(max_length=200, verbose_name=u"Text", null=True, blank=True)
+    image = models.ImageField(verbose_name=u"Image", null=True, blank=True)
+
+    class Meta:
+        ordering = ["-datetime"]
+
+    def __str__(self):
+        return "{}'s post".format(self.author.__str__())
--- ournft_site/ournft_site/settings.py
+++ ournft_site/ournft_site/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'ournft_app',
+    'ournft_site',
+    'accounts'
 ]
 
 MIDDLEWARE = [
