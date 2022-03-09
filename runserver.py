245d8d792742adcecc3c71df69136056f5e17ced
OddMood <oddmood.git@gmail.com>
2022-03-09 20:10:41 +0300

    Fix "table doesn't exist"

--- runserver.py
+++ runserver.py
@@ -4,6 +4,6 @@ env_python = "ournft-env\\Scripts\\python"
 
 run([env_python, "ournft_site/manage.py", "makemigrations"])
 
-run([env_python, "ournft_site/manage.py", "migrate"])
+run([env_python, "ournft_site/manage.py", "migrate", "--run-syncdb"])
 
 run([env_python, "ournft_site/manage.py", "runserver"])
