7f041d268daa32a6652ce8e760e428c0c45f0575
OddMood <oddmood.git@gmail.com>
2022-03-17 16:45:53 +0300

    Add ubuntu to setup and run scripts

--- runserver.py
+++ runserver.py
@@ -1,6 +1,11 @@
 from subprocess import run
+from sys import platform
+if platform.startswith('win32'):
+    os_python = "python"
+elif platform.startswith('linux'):
+    os_python = "python3"
 
-env_python = "ournft-env\\Scripts\\python"
+env_python = "ournft-env\\Scripts\\" + os_python
 
 run([env_python, "ournft_site/manage.py", "makemigrations"])
 
--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from subprocess import run
+from sys import platform
+if platform.startswith('win32'):
+    os_python = "python"
+elif platform.startswith('linux'):
+    os_python = "python3"
 
 run(["pip", "install", "virtualenv"])
 
-run(["python", "-m", "venv", "ournft-env"])
+run([os_python, "-m", "venv", "ournft-env"])
 
-env_python = "ournft-env\\Scripts\\python"
+env_python = "ournft-env\\Scripts\\" + os_python
 env_pip = "ournft-env\\Scripts\\pip"
 
 run([env_pip, "install", "-r", "requirements.txt"])
 
 run([env_python, "ournft_site/generate_secret.py"])
 
-run([env_python, "ournft_site/manage.py", "migrate"])
+run([env_python, "ournft_site/manage.py", "migrate", "--run-syncdb"])
