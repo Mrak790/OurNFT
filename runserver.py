bc3f601cd03d144a2860d71b07a7ff1b7469ef77
OddMood <oddmood.git@gmail.com>
2022-03-18 15:57:01 +0300

    Fix runserver.py

--- runserver.py
+++ runserver.py
@@ -1,11 +1,11 @@
 from subprocess import run
 from sys import platform
-if platform.startswith('win32'):
-    os_python = "python"
-elif platform.startswith('linux'):
-    os_python = "python3"
 
-env_python = "ournft-env\\Scripts\\" + os_python
+if platform.startswith('win32'):
+    env_python = "ournft-env/Scripts/python"
+
+elif platform.startswith('linux'):
+    env_python = "ournft-env/bin/python3"
 
 run([env_python, "ournft_site/manage.py", "makemigrations"])
 
