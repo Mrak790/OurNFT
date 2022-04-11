9b67235bed9748bf2755c320c99eebdc912abfc5
OddMood <oddmood.git@gmail.com>
2022-04-11 17:07:25 +0300

    Fix import in views (wtf otkuda on)

--- ournft_site/ournft_app/views.py
+++ ournft_site/ournft_app/views.py
@@ -1,4 +1,3 @@
-from msilib.schema import ListView
 from django.shortcuts import render, get_object_or_404, reverse
 from django.contrib.auth.models import User
 from .forms import ImageForm, RestoreImageForm, TransferForm, CommentForm
